54cb85b3a826e744b29bc70202f1369f931e1f89
jamesaud <jamaudre@indiana.edu>
2018-02-09 17:14:30 +0300

    spelling corrections

--- server/docker/entrypoint.sh
+++ server/docker/entrypoint.sh
@@ -4,7 +4,7 @@ if [ -d "./node_modules" ]; then
   echo "Skipping 'npm install' because 'node_modules already exists'. Delete 'node_modules' and restart the container if you want to reinstall binaries."
 
   else
-    echo "Installing dependencies (this will only run the first time your container is created. If 'node_modules' already exists, this will be skipped for subsequent cointainers)..."
+    echo "Installing dependencies (this will only run the first time your container is created. If 'node_modules' already exists, this will be skipped for subsequent containers)..."
     npm install
 fi
 
