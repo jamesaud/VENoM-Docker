77310fe05531561aa16d5ce12c5d69d768afc7cf
jamesaud <jamesaud@gmail.com>
2018-02-09 17:46:37 +0300

    Update entrypoint.sh

--- client/docker/entrypoint.sh
+++ client/docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -d "./node_modules" ]; then
   # Control will enter here if $DIRECTORY exists.
-  echo "Skipping 'npm install' because 'node_modules already exists'. Delete 'node_modules' and restart the container if you want to reinstall binaries."
+  echo "Skipping 'npm install' because 'node_modules' already exists. Delete 'node_modules' and restart the container if you want to reinstall binaries."
 
   else
     echo "Installing dependencies (this will only run the first time your container is created. If 'node_modules' already exists, this will be skipped for subsequent cointainers)..."
