5b4ec427158e86ead354e6818396e9472837ab83
jamesaud <jamaudre@indiana.edu>
2018-08-26 09:38:27 +0300

    few bug fixes

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default() => {
   return axios.create({
-    baseURL: process.env.API_URL || "http://localhost:8081/"
+    baseURL: process.env.API_URL || 'http://localhost:8081/'
   })
 }
